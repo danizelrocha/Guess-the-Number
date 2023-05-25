d53afe56b2099949c1fb2a47ca32c212b2227d14
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-05-25 09:05:48 -0300

    Lógica na busca
    Lógica na busca do resultado numérico

--- Antes/index.js
+++ Antes/index.js
@@ -10,4 +10,14 @@ function compareNumbers() {
     const userNumber = Number(document.getElementById('inputBox').value)
     userNumbers.push(' ' + userNumber)
     document.getElementById('guesses').innerHTML = userNumbers
+
+    if (userNumber > computerNumber) {
+        document.getElementById('textOutput').innerHTML = 'Seu número é Maior.'
+    }
+    else if (userNumber < computerNumber) {
+        document.getElementById('textOutput').innerHTML = 'Seu número é Menor.'
+    }
+    else {
+        document.getElementById('textOutput').innerHTML = 'Acertou Miserável :)'
+    }
 }
