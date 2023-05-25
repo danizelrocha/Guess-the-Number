166ae69e45a26833c82a7d3f7554354f21bb220d
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-05-25 08:45:20 -0300

    Campo Adivinha

---
+++ Antes/index.js
@@ -0,0 +1,13 @@
+let computerNumber
+let userNumbers =[]
+
+function init() {
+   computerNumber = Math.floor(Math.random() * 100 +1)
+   console.log(computerNumber)    
+}
+
+function compareNumbers() {
+    const userNumber = Number(document.getElementById('inputBox').value)
+    userNumbers.push(' ' + userNumber)
+    document.getElementById('guesses').innerHTML = userNumbers
+}
